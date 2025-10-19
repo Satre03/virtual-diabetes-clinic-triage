0d98e03e101eebd6a458f5751b471b67d4bf22c0
lov3lif3 <24396966+lov3lif3@users.noreply.github.com>
2025-10-19 21:19:48 +0200

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,8 @@
+
+
 from fastapi.testclient import TestClient
 from src.api import app
+
 
 def test_health():
     with TestClient(app) as client:
@@ -7,11 +10,19 @@ def test_health():
         assert res.status_code == 200
         assert res.json()["status"] == "ok"
 
+
 def test_prediction():
     payload = {
-        "age": 0.02, "sex": -0.044, "bmi": 0.06, "bp": -0.03,
-        "s1": -0.02, "s2": 0.03, "s3": -0.02, "s4": 0.02,
-        "s5": 0.02, "s6": -0.001
+        "age": 0.02,
+        "sex": -0.044,
+        "bmi": 0.06,
+        "bp": -0.03,
+        "s1": -0.02,
+        "s2": 0.03,
+        "s3": -0.02,
+        "s4": 0.02,
+        "s5": 0.02,
+        "s6": -0.001,
     }
     with TestClient(app) as client:
         res = client.post("/predict", json=payload)
